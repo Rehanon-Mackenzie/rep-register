c82e29bcb4408e42fc315154133241cfe314fce1
Rehanon-Mackenzie <fairywings38@hotmail.com>
2026-04-10 12:03:50 +0100

    add database

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_8XVUsBiGHW5N@ep-falling-sun-ag3g98py.c-2.eu-central-1.aws.neon.tech/tux_vegan_scope_699521"
+)
+
+os.environ.setdefault("SECRET_KEY", "sk_live_hMLybQQ7t37x6DhvkkeWJ3pryGjHmctg")
--- rep_register/settings.py
+++ rep_register/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import dj_database_url
+if os.path.isfile('env.py'): # This file does not exist on the deployed version
+    import env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&s(_k-l$g$)w4e8&k%m^w4*ob3xsk1f#elm2h@ec=v#1_m9x2%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com',
-'.127.0.0.1',]
+ALLOWED_HOSTS = ['.herokuapp.com', '.127.0.0.1',]
 
 
 # Application definition
@@ -75,13 +77,16 @@ WSGI_APPLICATION = 'rep_register.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
