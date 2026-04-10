6779dbb1832679c3caa88afd0d09a9a6fe96d118
Rehanon-Mackenzie <fairywings38@hotmail.com>
2026-04-10 15:31:53 +0100

    add  saved workouts view

---
+++ workouts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Workout
+
+
+class WorkoutForm(forms.ModelForm):
+    class Meta:
+        model = Workout
+        fields = ["exercise", "weight", "sets", "reps"]
--- workouts/views.py
+++ workouts/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import WorkoutForm
+
 # Create your views here.
+
+@login_required
 def index(request):
-    return HttpResponse("Hello, World!")
+    workouts = Workout.objects.filter(user=request.user)
+    
+    context = {
+        "workouts": workouts
+    }
+    return render(request,"workouts/index.html", context)
