75fd3c82ca545324505566180c06eb4f91aefde0
Rehanon-Mackenzie <fairywings38@hotmail.com>
2026-04-13 18:07:54 +0100

    add static files

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path # import path, similar to project's urls.py
 from . import views # import views.py from the current directory
 
+app_name = 'workouts'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path("add/", views.add_workout, name="add_workout"),
+    path("edit/<int:workout_id>/", views.edit_workout, name="edit_workout"),
+    path("delete/<int:workout_id>/", views.delete_workout, name="delete_workout"),
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .models import Workout
 from .forms import WorkoutForm
@@ -14,6 +16,7 @@ def index(request):
     }
     return render(request,"workouts/index.html", context)
 
+
 @login_required
 def add_workout(request):
     if request.method== "POST":
@@ -22,6 +25,34 @@ def add_workout(request):
             workout = form.save(commit=False)
             workout.user = request.user
             workout.save()
+            messages.add_message(request, messages.SUCCESS, 'Workout added!')
+            return redirect('workouts:index')
     else:
         form = WorkoutForm()
-    return render(request, "workouts/add_workout.html", {"form": form})
+    return render(request, 'workouts/add_workout.html', {'form': form})
+
+
+
+
+@login_required
+def edit_workout(request, workout_id):
+    workout = get_object_or_404(Workout, id=workout_id, user=request.user)
+    if request.method == "POST":
+        form = WorkoutForm(request.POST, instance=workout)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Workout edited!')
+            return redirect('workouts:index')
+    else:
+        form = WorkoutForm(instance=workout)
+    return render(request, 'workouts/edit_workout.html', {'form': form})
+
+
+@login_required
+def delete_workout(request, workout_id):
+    workout = get_object_or_404(Workout, id=workout_id, user=request.user)
+    if request.method == "POST":
+        workout.delete()
+        messages.add_message(request, messages.SUCCESS, 'Workout deleted!')
+        return redirect('workouts:index')
+    return redirect('workouts:index')
